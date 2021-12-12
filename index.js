44e25a82c73ecd4ce5fbb41c311a2f4b3ba5e835
bvds <bennyvds.stratx@gmail.com>
2021-12-12 14:36:24 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,52 @@
+
+function formattedNumber(nbr,digits=2){ //if no nbr of digits send use default 2
+    var formattedNbr = "";
+
+    if (typeof(nbr) === "string") { //in case a string is provided iso number, return the string
+        formattedNbr = nbr;
+    }
+
+    if ((typeof(nbr) === "number")) {
+        switch (digits) {
+            case 2:
+                if (nbr < 10) {                    
+                    formattedNbr = "0" + nbr;
+                }
+                else {
+                    formattedNbr = nbr;
+                }
+                break; 
+            case 3:
+                if (nbr < 10) {
+                    formattedNbr = "00" + nbr;
+                }
+                else {
+                    if (nbr < 100) {
+                        formattedNbr = "0" + nbr;
+                    }
+                    else {
+                        formattedNbr = nbr;
+                    }
+                }
+                break;
+            default: 
+                formattedNbr = nbr; 
+        }
+    }
+    return formattedNbr;
+}
+
+function printLL(_prefix,_msg){
+    var currentDT = new Date;
+    let dag = formattedNumber(currentDT.getDate());
+    let maand = formattedNumber(currentDT.getMonth()+1); //January is month 0 ...
+    let uur = formattedNumber(currentDT.getHours());
+    let minuten = formattedNumber(currentDT.getMinutes()); 
+    let sekonden = formattedNumber(currentDT.getSeconds());
+    let milisek = formattedNumber(currentDT.getMilliseconds(),3);
+    return _prefix + maand + "-" + dag + " " + uur +":"+minuten+":"+sekonden+":"+milisek+" "+_msg;
+}
+
+module.exports = printLL;
+
+
---
+++ index.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai'); //more advanced assert module
+const expect = chai.expect; //assign the expect flavor of chai to our constants. Can also be should or assert
+
+chai.use(require('chai-string')); //has methods like startsWidth
+
+const printLL = require('./index');
+
+describe('return fix formatted log msg', () => {
+    context('test the prefix and msg',()=>{
+        it('positive test with pref and ok',()=>{
+            expect(printLL("pref","ok")).to.startsWith("pref").endsWith("ok");
+        });
+        it('negative test with pref and ok',()=>{
+            expect(printLL("2pref","ok2")).to.not.startsWith("pref").endsWith("ok");
+        });
+    });
+    context('check the length',()=>{
+        it('check the length without pref or msg',()=>{
+            expect(printLL("","")).to.have.a.lengthOf(19);
+        });
+        it('check the length with single digit pref and msg',()=>{
+            expect(printLL("_","_")).to.have.a.lengthOf(21);
+        });
+    });  
+})
